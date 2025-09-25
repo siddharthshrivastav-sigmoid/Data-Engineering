3646dd904ed3fce49068f30047095cd6650f053a
Siddharth Shrivastav <siddharthshrivastav@Siddharths-MacBook-Air.local>
2025-09-25 21:18:14 +0530

    Made it more modular

--- main.py
+++ main.py
@@ -8,29 +8,31 @@ pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
-dotenv_path = Path('/Users/siddharthshrivastav/IdeaProjects/dataEngineer/secrets.env')
-load_dotenv(dotenv_path=dotenv_path)
+def fetch_data() -> pd.DataFrame:
+    dotenv_path = Path('/Users/siddharthshrivastav/IdeaProjects/dataEngineer/secrets.env')
+    load_dotenv(dotenv_path=dotenv_path)
 
-url = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"
+    url = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"
 
-headers = {
-    "accept": "application/json",
-    "Authorization": f"Bearer {os.getenv("API_READ_ACCESS_KEY")}"
-}
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.getenv("API_READ_ACCESS_KEY")}"
+    }
 
-response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-data = response.json()
+    data = response.json()
 
-movies = data['results']
+    movies = data['results']
 
-data = []
+    data = []
 
-for movie in movies:
-    row = [movie['title'],movie['popularity'],movie['release_date'],movie['vote_average'],movie['vote_count']]
-    data.append(row)
+    for movie in movies:
+        row = [movie['title'],movie['popularity'],movie['release_date'],movie['vote_average'],movie['vote_count']]
+        data.append(row)
 
-movie_df = pd.DataFrame(data, columns = ["title","popularity","release_date","vote_average","vote_count"])
-print(movie_df)
+    movie_df = pd.DataFrame(data, columns = ["title","popularity","release_date","vote_average","vote_count"])
+    return movie_df
 
-print(movie_df.describe().T)
+df = fetch_data()
+print(df)
